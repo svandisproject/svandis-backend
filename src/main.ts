0fd140f7e7bf10a6849b074d9c3d3a12d95d0aa6
Victor Wiebe <victorw@svandis.io>
2018-12-10 07:33:22 -0800

    Add port process for heroku deployment

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
         origin:  ['http://localhost:4200', 'https://app.svandis.io']});
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
